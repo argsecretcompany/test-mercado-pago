d7fde27a21fe0fee7fc8a7c386efed1dd54fd45d
isaiasleibo <isaias.leibovich@gmail.com>
2023-05-20 19:21:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+initMercadoPago('APP_USR-46e63112-b644-4e98-a54f-957a411776ae');
+
+const App = () => {
+  const [preferenceId, setPreferenceId] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const preference = {
+        items: [
+          {
+            title: "Mi producto",
+            unit_price: 10,
+            quantity: 1,
+          },
+        ],
+      };
+
+      try {
+        const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
+          method: "POST",
+          headers: {
+            Authorization: "Bearer APP_USR-8842171396041507-051919-9a7f455f0217de32eaf0374d60070293-1378960406",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(preference),
+        });
+
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+
+        const data = await response.json();
+        console.log('data',
+          data);
+        setPreferenceId(data.id);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  let setWallet = false;
+
+  if (preferenceId !== "" && !setWallet) {
+    setWallet = true;
+
+    return (
+      <Wallet initialization={{ preferenceId: preferenceId }} />
+    );
+  }
 }
 
 export default App;
